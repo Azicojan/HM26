fd5dd31b66e201b68d96c6c9fb19060d4cad6db7
Aziz Zulfikorov <tokzor123@yahoo.com>
2022-10-15 13:19:46 +0500

    this is my second commit to this file

--- Git_practice.js
+++ Git_practice.js
@@ -1,2 +1,3 @@
 console.log("hello,world");
-console.log("Hey, it's me again, digging deeper git and git hub")
+console.log("Hey, it's me again, digging deeper into git and github.")
+console.log("As you might have heard, the more you practice the better you become.")
