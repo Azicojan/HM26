5b009191de638af26f2ce39c9025e6fce86f152b
Aziz Zulfikorov <tokzor123@yahoo.com>
2022-10-15 13:34:46 +0500

    learning how to undo a commit

--- Git_practice.js
+++ Git_practice.js
@@ -1,3 +1,4 @@
 console.log("hello,world");
 console.log("Hey, it's me again, digging deeper into git and github.")
 console.log("As you might have heard, the more you practice the better you become.")
+console.log("These changes are going to be deleted.");
