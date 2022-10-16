d4b808ed574318450a2c69f22255bebdc2402136
Aziz Zulfikorov <tokzor123@yahoo.com>
2022-10-16 12:31:01 +0500

    getting a feel for undoing files in git

--- Git_practice.js
+++ Git_practice.js
@@ -2,3 +2,5 @@ console.log("hello,world");
 console.log("Hey, it's me again, digging deeper into git and github.")
 console.log("As you might have heard, the more you practice the better you become.")
 console.log("These changes are going to be deleted.");
+console.log("I'm learning how to undoing in git.");
+console.log("I'm trying to figure out how to undo staged files with git checkout.");
