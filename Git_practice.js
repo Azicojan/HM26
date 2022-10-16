0f96621c04a45d413f6017daa08251f4b62cb340
Aziz Zulfikorov <tokzor123@yahoo.com>
2022-10-16 16:32:33 +0500

    practicing git reset

--- Git_practice.js
+++ Git_practice.js
@@ -12,3 +12,4 @@ console.log(`I've confused a little bit 'git reset' with 'git checkout'. Well,
 'git reset' undoes staging a file but doesn't delete file, whereas 'git checkout' deletes
 the file completely. Also, '-- .' hasn't worked with 'git checkout', so 'git checkout
 <branch name> -- <file name> is the only way to delete a file as well that worked for me so far.'`);
+console.log("Now, I'm doing the task nine where it's asked to work <git reset>");
