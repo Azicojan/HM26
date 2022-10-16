1e3f8e52aa0d00d08c127de5787d2a95e2a6e15b
Aziz Zulfikorov <tokzor123@yahoo.com>
2022-10-16 13:14:19 +0500

    how to delete staged files

--- Git_practice.js
+++ Git_practice.js
@@ -4,3 +4,7 @@ console.log("As you might have heard, the more you practice the better you becom
 console.log("These changes are going to be deleted.");
 console.log("I'm learning how to undoing in git.");
 console.log("I'm trying to figure out how to undo staged files with git checkout.");
+console.log(`This message confirms that I figured out how to cancel all staged changes
+on your local file before committing them. First, you make some changes then add these 
+changes into stage area by applying 'git add'. In order to delete these changes, you 
+can use either 'git reset' or 'git checkout <branch name> -- <file name>, or -- .`);
